63abdd987a1ef64ccd38a55d9d6692047fc8569b
= <=>
2026-09-06 17:27:33 +0530

    Updated Everything

---
+++ src/main/java/com/demoApp/helloApp.java
@@ -0,0 +1,13 @@
+package com.demoApp;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class helloApp {
+	
+	@GetMapping("/hello")
+	public String Hello() {
+		return "Hello from CI/CD";
+	}
+}
